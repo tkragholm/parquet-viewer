f0e39ad622bcaa5d26b2fa7fb7b725cc8b84d785
Tobias Kragholm <50784887+tkragholm@users.noreply.github.com>
2024-12-19 13:28:12 +0100

    Fix Windows wheel building

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 from wheel.bdist_wheel import bdist_wheel
@@ -14,13 +17,14 @@ class BinaryDistWheel(bdist_wheel):
         python, abi, plat = super().get_tag()
         return "py3", "none", plat
 
-
 class InstallPlatlib(install):
     def finalize_options(self):
         super().finalize_options()
         # Force installation in platlib (platform-specific) directory
         self.install_lib = self.install_platlib
 
+# Determine the binary name based on the platform
+binary_name = 'parquet-viewer.exe' if sys.platform == 'win32' else 'parquet-viewer'
 
 setup(
     cmdclass={
@@ -31,7 +35,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     data_files=[
-        ("bin", ["bin/parquet-viewer"]),
+        ("bin", [os.path.join("bin", binary_name)]),
     ],
     scripts=["postinstall.py"],
 )
